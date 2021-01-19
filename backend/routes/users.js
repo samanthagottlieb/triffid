c47fff03e67dafc15b6dc94e5f4ef7e90ae67f57
Symion Edwards <symion.edwards@gmail.com>
2021-01-19 15:31:57 +0000

    Created backend server

---
+++ backend/routes/users.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+let User = require("../models/user.model");
+
+router.route("/").get((req, res) => {
+  User.find()
+    .then((users) => res.json(users))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+// User.find returns promise in JSON format. Then returns users in JSON format. If error will return status 400 error.
+
+router.route("/add").post((req, res) => {
+  const username = req.body.username;
+
+  const newUser = new User({ username });
+
+  newUser
+    .save()
+    .then(() => res.json("User added!"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+module.exports = router;
