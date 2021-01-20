407b6b7ea474ba792f05d901eda7f395e30dc41b
Samantha <samanthagottlieb31@gmail.com>
2021-01-20 16:25:40 +0000

    Update authentication so jwt required for access

--- backend/helpers/jwt.js
+++ backend/helpers/jwt.js
@@ -4,7 +4,9 @@ function authJwt() {
   const secret = process.env.secret;
   return expressJwt({
     secret,
-    algorithms: ["HS256"]
+    algorithms: ["HS256"],
+  }).unless({
+    path: ["/users/login"],
   });
 }
 
