73381908a1f7af4a220a6859077a4d99cd0edb91
Symion Edwards <symion.edwards@gmail.com>
2021-01-20 08:07:24 +0000

    Created a banner for the app to contain the logo and app name

--- App.js
+++ App.js
@@ -3,19 +3,19 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 //Screens
+import Header from "./Shared/Header";
 import PlantContainer from "./Screens/Plants/PlantContainer";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <PlantContainer />
+      <Header />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
--- Shared/Header.js
+++ Shared/Header.js
@@ -1,10 +1,33 @@
-// import React from "react";
-// import { StyleSheet, Image, SafeAreaView } from "react-native";
+import React from "react";
+import { StyleSheet, Text, Image, SafeAreaView } from "react-native";
 
-// const Header = () => {
-//   return(
-//     <View>
+const Header = () => {
+  return (
+    <SafeAreaView style={styles.header}>
+      <Text style={styles.text}>Triffid</Text>
+      <Image
+        source={require("../assets/favicon.png")}
+        resizeMode="contain"
+        style={[{ height: 55, marginBottom: 5 }]}
+      />
+    </SafeAreaView>
+  );
+};
 
-//     </View>
-//   )
-// }
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#52796F",
+    width: "100%",
+    flexDirection: "row",
+    alignContent: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 40,
+    fontWeight: "bold",
+    marginRight: 10,
+    color: "#CAD2C5",
+  },
+});
+
+export default Header;
