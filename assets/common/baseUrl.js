6378cff0e6db067374d5761a076635f02296c9d4
Samantha <samanthagottlieb31@gmail.com>
2021-01-23 17:01:28 +0000

    Correct base URLs

--- assets/common/baseUrl.js
+++ assets/common/baseUrl.js
@@ -4,8 +4,8 @@ let baseURL = "";
 
 {
   Platform.OS == "android"
-    ? (baseURL = "http://10.0.2.2:3000/api/v1/")
-    : (baseURL = "http://localhost:3000/api/v1");
+    ? (baseURL = "http://10.0.2.2:3000/")
+    : (baseURL = "http://localhost:3000/");
 }
 
 export default baseURL;
