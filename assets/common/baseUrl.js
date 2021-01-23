89a1e0415a457ee987c15d2eabd43babbe5ae94a
Samantha <samanthagottlieb31@gmail.com>
2021-01-23 17:33:06 +0000

    Correct base URL route

--- assets/common/baseUrl.js
+++ assets/common/baseUrl.js
@@ -4,8 +4,8 @@ let baseURL = "";
 
 {
   Platform.OS == "android"
-    ? (baseURL = "http://10.0.2.2:3000/")
-    : (baseURL = "http://localhost:3000/");
+    ? (baseURL = "http://10.0.2.2:5000/")
+    : (baseURL = "http://localhost:5000/");
 }
 
 export default baseURL;
