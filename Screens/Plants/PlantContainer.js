8ff6d0ac3c4ab7e9932c6ebfde3473b9f435207f
Symion Edwards <symion.edwards@gmail.com>
2021-01-23 10:01:27 +0000

    routing for plants

--- Screens/Plants/PlantContainer.js
+++ Screens/Plants/PlantContainer.js
@@ -8,6 +8,7 @@ import {
 } from "react-native";
 import PlantList from "./PlantList";
 import GreenPlusButton from "../../Components/GreenPlusButton";
+import { Row } from "native-base";
 
 //Flatlist Element is a list element with built in React Native features, such as pull to refresh.
 // renderItem({ item, index, separators });
@@ -26,25 +27,30 @@ const PlantContainer = (props) => {
   }, []);
 
   return (
-    // Change the padding here when the Navbar at the bottom is added!
-    <View style={styles.container}>
+    // Change the padding here when the Navbar at the bottom is added! <GreenPlusButton onPress={() => props.navigation.navigate("Add Plant")} />
+    <View styles={styles.buttonContainer}>
       <GreenPlusButton onPress={() => props.navigation.navigate("Add Plant")} />
-      <FlatList
-        showsVerticalScrollIndicator={false}
-        data={plants}
-        renderItem={({ item }) => (
-          <PlantList
-            navigation={props.navigation}
-            key={item.name}
-            item={item}
-          />
-        )}
-      />
+      <View style={styles.container}>
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          data={plants}
+          renderItem={({ item }) => (
+            <PlantList
+              navigation={props.navigation}
+              key={item.name}
+              item={item}
+            />
+          )}
+        />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    flexDirection: "row",
+  },
   container: {
     marginTop: 5,
     marginBottom: 25,
