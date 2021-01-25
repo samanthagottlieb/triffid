d35dab6bf18e55c47b0318353488795a6f2f4913
Symion Edwards <symion.edwards@gmail.com>
2021-01-25 13:29:35 +0000

    link sign up screen with server request

--- Screens/User/SignupPage.js
+++ Screens/User/SignupPage.js
@@ -2,11 +2,39 @@ import React, { useEffect, useContext, useState } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 import FormContainer from "../../Shared/Forms/FormContainer";
 import Input from "../../Shared/Forms/Input";
+import axios from "axios";
+import baseURL from "../../assets/common/baseUrl";
 
 const SignupPage = ({ navigation }) => {
   const [name, setName] = useState({ value: "", error: "" });
   const [email, setEmail] = useState({ value: "", error: "" });
   const [password, setPassword] = useState({ value: "", error: "" });
+
+  const handleSubmit = () => {
+    const user = {
+      name,
+      email,
+      password,
+    };
+
+    if (name === "" || email === "" || password === "") {
+      setError("Please fill in your email and password");
+    } else {
+      axios
+        .post(`${baseURL}users/add`, user)
+        .then((response) => {
+          console.log(response);
+        })
+        .then(
+          setTimeout(() => {
+            navigation.navigate("Login");
+          }, 500)
+        )
+        .catch((error) => {
+          console.log(`Error message: ${error}`);
+        });
+    }
+  };
 
   return (
     <FormContainer title={"Sign up"}>
@@ -16,7 +44,7 @@ const SignupPage = ({ navigation }) => {
         id={"name"}
         name={"name"}
         value={name}
-        onChangeText={(text) => setName(text.toLowerCase())}
+        onChangeText={(text) => setName(text)}
       />
       <Input
         label="Email"
@@ -36,7 +64,7 @@ const SignupPage = ({ navigation }) => {
         onChangeText={(text) => setPassword(text)}
       />
       <View style={styles.buttonGroup}>
-        <Button title="Sign up" />
+        <Button title="Sign up" onPress={() => handleSubmit()} />
       </View>
       <View style={[{ marginTop: 40 }, styles.buttonGroup]}>
         <Text style={styles.middleText}>Already have an account??</Text>
