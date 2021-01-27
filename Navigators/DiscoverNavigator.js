385dfda82b6a38d8ab87d180e3ba08f7f4b64013
Margarida Martelo <65411964+margmartelo@users.noreply.github.com>
2021-01-27 11:27:34 +0000

    Update DiscoverNavigator.js
    Deleting comments

--- Navigators/DiscoverNavigator.js
+++ Navigators/DiscoverNavigator.js
@@ -4,10 +4,6 @@ import SearchPlants from '../Screens/Plants/SearchPlants';
 
 const DiscoverStack = createStackNavigator();
 
-//defines clases ---> class PrimerSegundoTercer
-//instancias variables --> const primerSegundoTercer // let primerSegundoTercer;
-//Constante            --> const TOKEN_AUTH = 'adfasdf'
-
 function DiscoverStackScreen() {
     return (
         <DiscoverStack.Navigator>
