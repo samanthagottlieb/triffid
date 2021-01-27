09b102732567de8e6698951cf00f98f569a65f0a
Samantha <samanthagottlieb31@gmail.com>
2021-01-27 11:48:19 +0000

    Separate out connection to database and port from main server
    Preventing connecting to database when running tests, in order to avoid dropping collections

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,9 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const mongoose = require("mongoose");
-require("dotenv").config();
 const authJwt = require("./helpers/jwt");
 const errorHandler = require("./helpers/error-handler");
-const port = process.env.PORT || 5000;
+require("dotenv").config();
 
 //Middleware
 app.use(cors());
@@ -14,18 +12,6 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(authJwt());
 app.use(errorHandler);
-
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
-
-const connection = mongoose.connection;
-connection.once("open", () => {
-  console.log("MongoDB database connection established successfully");
-});
 
 const plantsRouter = require("./routes/plants");
 const usersRouter = require("./routes/users");
@@ -35,6 +21,4 @@ app.use("/plants", plantsRouter);
 app.use("/users", usersRouter);
 // Whenever someone goes to root url / exercises it'll reditect to users router etc.
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+module.exports = app;
