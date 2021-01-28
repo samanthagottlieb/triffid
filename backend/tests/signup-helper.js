343c84e4d6ab05067a546f1d08b0251dd660db86
Samantha <samanthagottlieb31@gmail.com>
2021-01-28 15:56:06 +0000

    Rename test helper to be more descriptive

---
+++ backend/tests/signup-helper.js
@@ -0,0 +1,16 @@
+const app = require("../server");
+const supertest = require("supertest");
+const request = supertest(app);
+
+async function createUser() {
+  return await request
+    .post("/users/add")
+    .send({
+      name: "Wesley",
+      email: "wesley@example.com",
+      password: "wesley",
+    })
+    .set("Accept", "application/json");
+}
+
+module.exports = { createUser };
