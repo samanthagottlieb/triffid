dc1c43db9d905f7658c2daf6983cdf97393a93f9
Symion Edwards <symion.edwards@gmail.com>
2021-02-18 11:27:34 +0000

    Add plant test passing

---
+++ backend/tests/plant.model.test.js
@@ -0,0 +1,45 @@
+const app = require("../server");
+const supertest = require("supertest");
+const request = supertest(app);
+const createUser = require("./signup-helper").createUser;
+const dbHandler = require("./db-handler");
+const authTests = require("./auth-helpers");
+const authJwt = require("../helpers/jwt");
+require("dotenv").config();
+const UserModel = require("../models/user.model");
+
+beforeAll(async () => await dbConnect());
+afterEach(async () => await dbClear());
+afterAll(async () => await dbClose());
+
+describe("Plant", () => {
+  describe("Add", () => {
+    it("A user can successfully add a plant to their terrarium", async () => {
+      let user = await createUser();
+      let parsedUser = JSON.parse(user.text);
+      let userLoggedIn = await logInUser();
+      let parsedUserLoggedIn = JSON.parse(userLoggedIn.text);
+      let wesleyToken = parsedUserLoggedIn.token;
+      await request
+        .post("/plants/add")
+        .send({
+          userid: parsedUser._id,
+          nickname: "Stina",
+          type: "plant",
+          lastWatered: Date.now(),
+          wateringFrequency: 7,
+          notes: "hello!",
+        })
+        .set("Authorization", `Bearer ${wesleyToken}`)
+        .then((response) => {
+          expect(response.statusCode).toBe(200);
+          expect(response.type).toBe("application/json");
+        });
+    });
+  });
+});
+
+// it("Gives the right server response", async () => {
+//   let user = await createUser();
+//   expect(user.status).toEqual(200);
+// });
